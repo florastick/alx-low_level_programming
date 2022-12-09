ca4d0f4fdd7a383bd7cf631ac4d47e5c42edccae
John <darealpiece@gmail.com>
2022-12-08 20:21:40 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print C program using printf
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
