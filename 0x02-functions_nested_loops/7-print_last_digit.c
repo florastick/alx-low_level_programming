03aa378384f21ff54528979cb9f17c9740040509
John <darealpiece@gmail.com>
2022-12-14 20:38:57 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,8 +10,12 @@
 
 int print_last_digit(int n)
 {
+	int last = n % 10;
+
 	if (n < 0)
-		n = n * -1;
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	last = last * -1;
+
+	_putchar(last + '0');
+
+	return (last);
 }
