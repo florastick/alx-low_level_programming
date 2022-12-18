6839c2df152173120a8bd3539a6739edd707276c
John <darealpiece@gmail.com>
2022-12-18 11:38:27 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,24 +1,21 @@
 #include "main.h"
 
 /**
- * print_line - Draws a straight line using a character _.
- * @n: The numbers of _ characters to be printed out
- * Return: Always 0.
+ * print_line - check for a digit
+ * @n : number of _ to be printed
+ * Return: void
 */
+
 void print_line(int n)
 {
-	int count = 0;
 
-	if (n <= 0)
-		_putchar('\n');
-	else
-	{
-		while (c < n)
+	int i = 0;
+
+	while (i < n && n > 0)
 	{
 		_putchar('_');
-		c++;
+		i++;
 	}
 	_putchar('\n');
-	}
 }
 
