51538e07393bb9bed2c6048f4fb778f5c13f69b7
John <darealpiece@gmail.com>
2022-12-18 21:53:39 -0800

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size : integer type
+ * Return: Always 0.
+*/
+
+void print_triangle(int size)
+{
+	int i = 1, ii;
+
+	while (i <= size && size > 0)
+	{
+		ii = 0;
+		while (ii < -i)
+		{
+			_putchar(' ');
+			ii++;
+		}
+		ii = 0;
+		while (ii < i)
+		{
+			_putchar('#');
+			ii++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+	if (i == 1)
+{
+		_putchar('\n');
+}
+}
+
