6bf167e9357c18bebb2fd3399fc2ca0dcbb34fd5
John <darealpiece@gmail.com>
2023-01-06 14:49:02 -0800

    task 0

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character.
+ * @c: character to be checked
+ *
+ * Return: 1 if @c is 0
+ * Otherwise, we return 0
+*/
+
+int _isupper(int c)
+{
+	int start = 65, end = 90;
+
+	if (c >= start && c <= end)
+		return (1);
+	return (0);
+}
