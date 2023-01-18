019a090c3186f8ad2962979e2219b4823301940d
John <darealpiece@gmail.com>
2023-01-18 09:40:05 -0800

    function task

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,41 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include<stdio.h>
+#include<stdlib.h>
 /**
- * main - check the code for Holberton School students.
- * @argc: argument count.
- * @argv: argument vector.
- *
- * Return: Always 0.
- */
+ * main- Entry point
+(* a blank line
+ *@argc: the number of parameters.
+ *@argv: the parameeters in the case the number ob bytes.
+* Description: this program prints opcodes in hexa)?
+* Return: 0 in succes
+*/
 int main(int argc, char *argv[])
 {
-	char *opc = (char *) main;
-	int i, nbytes;
+	int i, n;
+
 
 	if (argc != 2)
 	{
-	printf("Error\n");
-	exit(1);
+		printf("Error\n");
+		return (1);
 	}
-
-	char *opc = (char *) main;
-
-	nbytes = atoi(argv[1]);
-
-	if (nbytes < 0)
+	n = atoi(argv[1]);
+	if (n < 0)
 	{
-	printf("Error\n");
-	exit(2);
+		printf("Error\n");
+		exit(2);
 	}
 
-	for (i = 0; i < nbytes; i++)
+	for (i = 0; i < n; i++)
 	{
-	printf("%02x", opc[i] & 0xFF);
-	if (i != nbytes - 1)
-	printf(" ");
+		printf("%02hhx", *((char *)main + i));
+		if (i < n - 1)
+			printf(" ");
+		else
+			printf("\n");
 	}
-
-	printf("\n");
 	return (0);
 }
