d4cc82252e715c2e8021440254bab30dc836d9ba
John <darealpiece@gmail.com>
2023-01-19 08:38:33 -0800

    variadic tasks

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+
+#include <stdarg.h>
+#include <stdio.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void _putchar(char c);
+
+#endif /* _VARIADIC_FUNCTIONS_H_ */
